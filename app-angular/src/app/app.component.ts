14bc5a85e50407d601e4f1e50bebd0211f69d9e4
alejandro quimis <alejandro.quimis.3@gmail.com>
2020-01-31 10:26:20 -0500

    directivas de atributos

--- app-angular/src/app/app.component.ts
+++ app-angular/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-angular';
+  show: boolean;
+  color: string[];
+
+  ngOnInit(): void {
+      this.show = true;
+      this.color = ['red','green','blue','yellow'];
+  }
 }
